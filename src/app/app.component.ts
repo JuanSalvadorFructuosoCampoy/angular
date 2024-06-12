3c3e84ad95225c06f8e253e84b42ffd1ce75b1a6
Juansa <juansacampoy.train@gmail.com>
2024-06-12 14:04:30 +0200

    Añadido componente Empleados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { EmpleadosComponent } from './empleados/empleados.component';
 @Component({
-  selector: 'app-root',
+  selector: 'web-principal',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, EmpleadosComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
-  title = 'myapp';
+  title = 'web-principal';
+  nombre = 'Juansa';
 }
