45d679b44b7dd95796ef415116820a20dd18de12
Juansa <juansacampoy.train@gmail.com>
2024-06-13 20:25:34 +0200

    Event binding, con radios cambiando un texto

--- src/app/empleado/empleado.component.ts
+++ src/app/empleado/empleado.component.ts
@@ -17,19 +17,27 @@ export class EmpleadoComponent {
   usuRegistrado = false; //Esta propiedad debe bindearse con la propiedad del cuadro de texto del template empleado.component.html
 
   textoRegistro = "No hay nadie registrado";
-  
+
   getEdad() { return this.edad; }
 
-  cambiaEmpresa(value: string) {}
-  
+  cambiaEmpresa(value: string) { }
+
   getRegistroUsuario() {
     this.usuRegistrado = false;
   }
 
-  setUsuarioRegistrado() {
-    // alert("El usuario se ha registrado correctamente");//Creamos la función que se va a activar cuando hagamos click en el checkbox
-    this.textoRegistro = "El usuario se ha registrado"; //Establecemos el valor de la propiedad textoRegistro para que se muestre en el template al hacer click en el checkbox
-  }
-  //Llamamos a la función en el template y establecemos el valor de usuRegistrado, así se actualiza la checkbox
+  //Para los parámetros, la notación es la siguiente: nombreParametro: tipoDato
+  setUsuarioRegistrado(event: Event) {
+    //Event.target hace referencia al elemento HTML que ha disparado el evento
+    // alert(event.target); //Mostramos el tipo de objeto en la alerta. Con console.log no podemos ver el tipo de objeto, mientras que con alert sí. Esto es útil para castearlo en el if de abajo
 
+    // alert("El usuario se ha registrado correctamente");//Creamos la función que se va a activar cuando hagamos click en el checkbox
+    //this.textoRegistro = "El usuario se ha registrado"; //Establecemos el valor de la propiedad textoRegistro para que se muestre en el template al hacer click en el checkbox
+    if ((<HTMLInputElement>event.target).value == "si") { //Lo casteamos a HTMLInputElement para que TypeScript no nos de error, ya que al usar event.target obtenemos que es de tipo HTMLInputElement
+      this.textoRegistro = "El usuario se ha registrado";
+    } else {
+      this.textoRegistro = "No hay nadie registrado";
+    }
+    //Llamamos a la función en el template y establecemos el valor de usuRegistrado, así se actualiza la checkbox
+  }
 }
