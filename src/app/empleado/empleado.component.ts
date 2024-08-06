bb2190912c26a06979dce03730329aa0d5041a64
Juansa <juansacampoy.train@gmail.com>
2024-08-06 11:30:41 +0200

    Revert "Binding bidireccional con ngModel y Banana in a Box"
    This reverts commit b1bc10a3fc7b984f84c8f4f78f5c60bd1c0b9d42.

--- src/app/empleado/empleado.component.ts
+++ src/app/empleado/empleado.component.ts
@@ -1,37 +1,43 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';//Añadimos esto para poder usar el ngModel en el template empleado.component.html, usando el binding bidireccional
 
 @Component({
-  selector: 'app-empleado',
+  selector: 'app-empleado', //Este es el nombre que debe tener la etiqueta HTML para que se muestre el contenido de este componente
   standalone: true,
-  imports: [FormsModule], //Añadimos FormsModule para poder usar el ngModel en el template empleado.component.html usando el binding bidireccional
+  imports: [],
   templateUrl: './empleado.component.html',
   styleUrl: './empleado.component.css'
 })
 export class EmpleadoComponent {
   nombre = "Juansa";
   apellido = "Campoy";
-  private edad = 33;
-  empresa = "Google"; //El valor de la empresa ahora va en dos direcciones: se verá en el texto y en el input, gracias al binding bidireccional
+  private edad = 33; //Los modificadores de acceso private, public y protected se pueden usar aquí
+  //empresa = "Freelance";
   mensajEdad = "";
-  habilitacionCuadro = true;
-  usuRegistrado = false;
+  habilitacionCuadro = true; //Esta propiedad debe bindearse con la propiedad del cuadro de texto del template empleado.component.html
+  usuRegistrado = false; //Esta propiedad debe bindearse con la propiedad del cuadro de texto del template empleado.component.html
 
   textoRegistro = "No hay nadie registrado";
 
   getEdad() { return this.edad; }
 
+  cambiaEmpresa(value: string) { }
+
   getRegistroUsuario() {
     this.usuRegistrado = false;
   }
 
-  
+  //Para los parámetros, la notación es la siguiente: nombreParametro: tipoDato
   setUsuarioRegistrado(event: Event) {
-   
-    if ((<HTMLInputElement>event.target).value == "si") { 
+    //Event.target hace referencia al elemento HTML que ha disparado el evento
+    // alert(event.target); //Mostramos el tipo de objeto en la alerta. Con console.log no podemos ver el tipo de objeto, mientras que con alert sí. Esto es útil para castearlo en el if de abajo
+
+    // alert("El usuario se ha registrado correctamente");//Creamos la función que se va a activar cuando hagamos click en el checkbox
+    //this.textoRegistro = "El usuario se ha registrado"; //Establecemos el valor de la propiedad textoRegistro para que se muestre en el template al hacer click en el checkbox
+    if ((<HTMLInputElement>event.target).value == "si") { //Lo casteamos a HTMLInputElement para que TypeScript no nos de error, ya que al usar event.target obtenemos que es de tipo HTMLInputElement
       this.textoRegistro = "El usuario se ha registrado";
     } else {
       this.textoRegistro = "No hay nadie registrado";
     }
+    //Llamamos a la función en el template y establecemos el valor de usuRegistrado, así se actualiza la checkbox
   }
 }
